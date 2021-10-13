668dfecde2fdc23badad0a22ea3132eee45f3491
Joseph Percival <joseph.m.percivaL@gmail.com>
2021-10-13 22:51:43 +0100

    structured the index hero section by adding a column for the image and made responsive

--- scripts/dynamic-script.js
+++ scripts/dynamic-script.js
@@ -1,9 +1,5 @@
-// window.addEventListener("load", function(){
-
 // Hero text (index page) 
-
 var TxtRotate = function (el, toRotate, period) {
-  "use strict";
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
@@ -14,57 +10,58 @@ var TxtRotate = function (el, toRotate, period) {
 };
 
 TxtRotate.prototype.tick = function () {
-  "use strict";
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
 
   if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
   this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
   var that = this;
   var delta = 150 - Math.random() * 100;
+
   if (this.isDeleting) { delta /= 2; }
+
   if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
+    delta = this.period;
+    this.isDeleting = true;
   } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      // this.loopNum++;
-      delta = 500;
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
   }
 
   setTimeout(function () {
-      that.tick();
+    that.tick();
   }, delta);
 };
 
 
 var elements = document.getElementsByClassName('txt-rotate');
 for (var i = 0; i < elements.length; i++) {
-var toRotate = elements[i].getAttribute('data-rotate');
-var period = elements[i].getAttribute('data-period');
-if (toRotate) {
-  TxtRotate(elements[i], JSON.parse(toRotate), period);
-}
+  var toRotate = elements[i].getAttribute('data-rotate');
+  var period = elements[i].getAttribute('data-period');
+  if (toRotate) {
+    new TxtRotate(elements[i], JSON.parse(toRotate), period);
+  }
 }
 // INJECT CSS
 const style = document.createElement('style');
 style.innerHTML = `
-.txt-rotate > .wrap {
-      border-right: 0.1em solid #2c4465;
-      font-family: 'Poppins';
-      font-size: 1.5em;
-      font-weight: 600;
-      transform: none;
-      color: #2c4465;
-      margin: 0;
-    }
-  `;
+  .txt-rotate > .wrap {
+        border-right: 0.1em solid #2c4465;
+        font-family: 'Poppins';
+        font-size: 1.5em;
+        font-weight: 600;
+        transform: none;
+        color: #2c4465;
+        margin: 0;
+      }
+    `;
 document.head.appendChild(style);
 
 //   Modal toggle 
